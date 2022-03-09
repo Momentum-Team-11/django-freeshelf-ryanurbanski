a9df96d15390b196ab45bf5dc0f22ac63e27ef79
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-03-09 11:25:17 -0500

    Book model added and migrated successfully.

--- books/models.py
+++ books/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import datetime
+# import csv
+from django.utils.text import slugify    
 
 
 class User(AbstractUser):
@@ -8,3 +11,11 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+    url = models.URLField(max_length=200)
+    date = models.DateTimeField(auto_now_add=datetime.now)
+    # pic = models.ImageField(blank=True)
--- books/views.py
+++ books/views.py
@@ -1,3 +1,19 @@
+from typing import List
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+# from .forms import NotesForm
+from .models import Books
+
+class BooksListView(LoginRequiredMixin, ListView):
+    model = Books
+    context_object_name = "books"
+    template_name = "books/books_list.html"
+    login_url = "/admin"
+
+    def get_queryset(self):
+        return self.request.user.books.all()
