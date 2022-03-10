8cc01b29fe7f12a4c505b6483731714497919362
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-03-10 00:28:41 -0500

    Admin working again.

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import datetime
 
 
 class User(AbstractUser):
@@ -8,3 +9,10 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    url = models.URLField(max_length=200)
+    description = models.CharField(max_length=2000)
+    date = models.DateTimeField(auto_now_add=datetime.now)
