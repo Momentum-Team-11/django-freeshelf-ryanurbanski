f0865f1e6f906833d22c2558c79430214d6e9a7f
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-03-10 00:46:42 -0500

    created homepage

--- books/views.py
+++ books/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from http.client import CREATED
+from unicodedata import category
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
+from .models import Book, User, Comment
+from django.contrib.auth.decorators import login_required
+from django.db import models
+from django.contrib.auth.views import LoginView
+
+
+
+def homepage(request):
+    if request.user.is_authenticated:
+        return redirect("book_list")
+    return render(request, "home.html")   
+    
+    
+def book_list(request):
+    books = Book.objects.all().order_by("date")
+    return render(request, "book_list.html", {"books": books})
