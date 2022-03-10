f41a182f0b835215cb61d01175d9d0b22d11e421
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-03-09 21:49:29 -0500

    Book list view page displaying with header.

--- books/urls.py
+++ books/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books', views.BooksListView.asView(), name="books.list"),
+    path('', views.BooksListView.as_view()),
+    
 ]
